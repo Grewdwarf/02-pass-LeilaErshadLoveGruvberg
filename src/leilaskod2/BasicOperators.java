089d87c2c2de2ae141f9ad0cbca61a46ad888885
Love Gruvberg <Grooveberg@Loves-MacBook-Pro.local>
2021-03-30 14:02:26 +0200

    Hej?

---
+++ src/leilaskod2/BasicOperators.java
@@ -0,0 +1,12 @@
+package leilaskod2;
+
+public interface BasicOperators { // BASICOPERATOR
+
+	public double add(double operand1, double operand2);
+
+	public double sub(double operand1, double operand2);
+
+	public double mult(double operand1, double operand2);
+
+	public double div(double operand1, double operand2);
+}
---
+++ src/leilaskod2/MyCalcTest2.java
@@ -0,0 +1,8 @@
+package leilaskod2;
+
+public class MyCalcTest2 extends MyCalculator {
+	public static void main(String[] args) {
+		MyCalcTest2 c = new MyCalcTest2();
+		c.runMyCalculator();
+	}
+}
